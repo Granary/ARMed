13a729e9c01b82f655212a848d9db25b6515be7e
kumarak <iit.akshay@gmail.com>
2014-04-21 21:58:19 -0700

    initial commit; some files missing; no Makefile

---
+++ decode_fast.c
@@ -0,0 +1,51 @@
+/*
+ * decode_fast.c
+ *
+ *  Created on: 2014-03-30
+ *      Author: akshayk
+ */
+
+#include "global.h"
+#include "instr.h"
+
+typedef unsigned char byte;
+
+#define SIZE_SZ     4
+
+int
+decode_sizeof(byte *start_pc)
+{
+    return SIZE_SZ;
+}
+
+byte *
+decode_cti(byte *pc, instr_t *instr)
+{
+    /*TODO : check decoding of CTI instructions*/
+    return 0;
+}
+
+
+/* Returns a pointer to the pc of the next instruction
+ * Returns NULL on decoding an invalid instruction.
+ */
+byte *
+decode_next_pc(byte *pc)
+{
+    int sz = decode_sizeof(pc);
+    if (sz == 0)
+        return NULL;
+    else
+        return pc + sz;
+}
+
+
+byte *
+decode_raw(byte *pc, instr_t *instr)
+{
+    int sz = decode_sizeof(pc);
+    return (byte*)(pc + sz);
+}
+
+
+
---
+++ global.h
@@ -0,0 +1,78 @@
+/*
+ * global.h
+ *
+ *  Created on: 2014-03-30
+ *      Author: akshayk
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CLIENT_ASSERT(...)
+#define ASSERT(...)
+
+#ifndef true
+#   define true 1
+#endif
+
+#ifndef false
+#   define false 0
+#endif
+
+typedef int bool;
+
+typedef unsigned int uint;
+
+typedef unsigned short ushort;
+
+typedef unsigned char byte;
+
+typedef signed char sbyte;
+
+typedef byte * app_pc;
+
+typedef void (*generic_func_t) ();
+
+typedef unsigned long long int uint64;
+
+typedef uint64 reg_t;
+
+typedef long long int int64;
+
+typedef int ptr_int_t;
+
+
+struct _opnd_t;
+typedef struct _opnd_t opnd_t;
+struct _instr_t;
+typedef struct _instr_t instr_t;
+struct _instr_list_t;
+struct _fragment_t;
+typedef struct _fragment_t fragment_t;
+struct _future_fragment_t;
+typedef struct _future_fragment_t future_fragment_t;
+struct _trace_t;
+typedef struct _trace_t trace_t;
+struct _linkstub_t;
+typedef struct _linkstub_t linkstub_t;
+struct _dcontext_t;
+typedef struct _dcontext_t dcontext_t;
+struct vm_area_vector_t;
+typedef struct vm_area_vector_t vm_area_vector_t;
+struct _coarse_info_t;
+typedef struct _coarse_info_t coarse_info_t;
+struct _coarse_freeze_info_t;
+typedef struct _coarse_freeze_info_t coarse_freeze_info_t;
+struct _module_data_t;
+typedef struct _module_data_t module_data_t;
+typedef struct _instr_list_t instrlist_t;
+typedef struct _module_data_t module_data_t;
+
+
+
+
+#endif /* GLOBAL_H_ */
